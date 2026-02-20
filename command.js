2affc6a03fafc21d9526fdb9fc510811c366c6f5
MR. 254 <alexmwachemba44@gmail.com>
2026-02-21 00:13:21 +0300

    Refactor command.js to use const instead of var

--- command.js
+++ command.js
@@ -1,21 +1,18 @@
-// DARKZONE-MD 
-var commands = [];
+const commands = [];
 
 function cmd(info, func) {
-    var data = info;
+    const data = info;
     data.function = func;
     if (!data.dontAddCommandList) data.dontAddCommandList = false;
-    if (!info.desc) info.desc = '';
+    if (!data.desc) data.desc = '';
     if (!data.fromMe) data.fromMe = false;
-    if (!info.category) data.category = 'misc';
-    if(!info.filename) data.filename = "Not Provided";
+    if (!data.category) data.category = 'misc';
+    if (!data.filename) data.filename = "Not Provided";
     commands.push(data);
     return data;
 }
+
 module.exports = {
     cmd,
-    AddCommand:cmd,
-    Function:cmd,
-    Module:cmd,
-    commands,
+    commands
 };
